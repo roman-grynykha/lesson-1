5f994340eefb46311682e3362519b421112516ee
Roman Grynykha <roman.grynykha@p2h.com>
2023-06-16 07:49:28 +0300

    Initial commit

---
+++ routes/web.php
@@ -0,0 +1,85 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider and all of them will
+| be assigned to the "web" middleware group. Make something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+
+/* ------------------------------------------------------------------------
+ * 9 | Створіть маршрут “/welcome”, який повертає текст
+ * “Welcome to our website!“.
+ */
+Route::get('/welcome', function () {
+    return 'Welcome to our website!';
+});
+
+
+/* ------------------------------------------------------------------------
+ * 10 | Створіть маршрут “/welcome/{name}“, який повертає “Welcome, {name}!“.
+ * Додайте перевірку на відсутність імені. Якщо ім‘я відсутнє, маршрут
+ * має повертати “Welcome, guest”.
+ *
+ >>>> При відсутності $name, конфліктує з роутом '/welcome'
+ >>>> Відпрацює перший оголошений
+ */
+Route::get('/welcome/{name?}', function (string $name = 'guest') {
+    return  "Welcome, {$name}!";
+});
+
+
+/* ------------------------------------------------------------------------
+ * 11 | Створіть маршрут “/welcome/{name}/{age}“, який повертає
+ * “Welcome, {name}. You are {age} years old.“. Додайте перевірку, що вік є числом.
+ * Якщо вік не є числом, маршрут має повертати помилку.
+ */
+Route::get('/welcome/{name}/{age}', function (string $name, int $age) {
+    return  "Welcome, {$name}. You are {$age} years old.";
+})->whereNumber("age");
+
+
+/* ------------------------------------------------------------------------
+ * 12 | Створіть маршрут “/year/{year}“, який перевіряє, чи рік дорівнює поточному року.
+ * Якщо так, то повертайте “Yes, this is the current year”.
+ * Якщо ні, то “No, this is not the current year”.
+ */
+Route::get('/year/{year}', function (int $year) {
+    return (in_array( $year, [date('Y'), date('y')]))
+        ? "Yes, this is the current year"
+        : "No, this is not the current year";
+})->whereNumber("year");
+
+
+/* ------------------------------------------------------------------------
+ * 13 | Створіть маршрут “/age/{age}“, який повертає “You are {age} years old.“.
+ * Зробіть так, щоб маршрут працював тільки для числових значень віку.
+ */
+Route::get('/age/{age}', function (int $age) {
+    return "You are {$age} years old.";
+})->whereNumber("age");
+
+
+/* ------------------------------------------------------------------------
+ * 14 | Створіть маршрут “/find”, який приймає параметр “q” і повертає
+ * “You are searching for {q}“. Додайте перевірку, що параметр “q” не є пустим.
+ * Якщо параметр “q” пустий, повертайте “Please enter a search query”.
+ */
+Route::get('/find', function () {
+    $q = request()->query("q");
+    return ($q)
+        ? "You are searching for {$q}"
+        : "Please enter a search query";
+});
+
